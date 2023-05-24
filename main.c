a77c58ca5410e59d283359350963061c349ffd6c
Sandra Mabonga <sandramabonga303@gmail.com>
2023-05-24 01:00:58 -0700

    prompt

---
+++ main.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * main -shell
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	char *input = NULL, *prompt = "$ ", *cmd[50];
+	size_t len = 0;
+	ssize_t nread = 0;
+
+	do
+	{
+		write(STDOUT_FILENO, prompt, strlen(prompt));
+		nread = getline(&input, &len, stdin);
+		if (!nread)
+			return (-1);
+		if (input[0] == '\n')
+		{
+			free(input);
+			continue;
+		}
+		if (strcmp(input, "exit\n") == 0)
+		{
+			free(input);
+			exit(EXIT_SUCCESS);
+		}
+		free(input);
+		input = NULL;
+		len = 0;
+	}
+	while(1);
+
+	return (0);
+}
+

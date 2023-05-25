06b2656e6aade74815c7b289e4f25dd2226aedf3
edoka <peter.edoka.242464@unn.edu.ng>
2023-05-25 12:49:04 +0100

    fix betty error on main.h

--- main.h
+++ main.h
@@ -12,6 +12,8 @@ void parse_input(char *input, char **cmd);
 void print_env(char *envp[]);
 char *get_path(char *input, char **envp);
 void free_input(char **cmd);
+int _execve(char *path, char **cmd, char **env,
+	char **argv, char *input, int not_pipe);
 
 /* string functions */
 char *_strcat(char *dest, char *src);
@@ -20,7 +22,6 @@ char *_strcpy(char *dest, char *src);
 unsigned int _strcspn(char *s, char *reject);
 char *_strdup(char *str);
 int _strlen(char *s);
-int _execve(char *path, char **cmd, char **env, char **argv, char *input, int not_pipe);
 
 
 #endif /* MAIN_H */
